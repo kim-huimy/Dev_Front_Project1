597c602458f631ca51084b8fe02687c7782810b0
kim-huimy <kim-huimy@users.noreply.github.com>
2020-03-10 12:13:12 +0100

    maj 10/03

---
+++ annuaire.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Annuaire B1</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" rel="stylesheet">
+        <link href="assets/css/style.css" rel="stylesheet">
+        <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+    </head>
+    <body class="annuaire">
+        <div class="container">
+            <header class="row">
+                <h1 class="col-12">Annuaire B1</h1>
+            </header>
+            <main class="row">
+                <nav class="col-12">
+                    <ul class="nav">
+                        <li class="nav-item">Tous</li>
+                        <li class="nav-item">A</li>
+                        <li class="nav-item">B</li>
+                        <li class="nav-item">C</li>
+                        <li class="nav-item">D</li>
+                        <li class="nav-item">E</li>
+                        <li class="nav-item">F</li>
+                        <li class="nav-item">G</li>
+                        <li class="nav-item">H</li>
+                        <li class="nav-item">I</li>
+                        <li class="nav-item">J</li>
+                        <li class="nav-item">K</li>
+                        <li class="nav-item">L</li>
+                        <li class="nav-item">M</li>
+                        <li class="nav-item">N</li>
+                        <li class="nav-item">O</li>
+                        <li class="nav-item">P</li>
+                        <li class="nav-item">Q</li>
+                        <li class="nav-item">R</li>
+                        <li class="nav-item">S</li>
+                        <li class="nav-item">T</li>
+                        <li class="nav-item">U</li>
+                        <li class="nav-item">V</li>
+                        <li class="nav-item">W</li>
+                        <li class="nav-item">X</li>
+                        <li class="nav-item">Y</li>
+                        <li class="nav-item">Z</li>
+                    </ul>
+                </nav>
+                <select class="col-6" id="categories">
+                    <option value="all" selected>Toutes les catégories</option>
+                    <option value="webdesign">Webdesin</option>
+                    <option value="webmarketing">Webmarketing</option>
+                    <option value="developpement">Développement</option>
+                    <option value="design">Design</option>
+                </select>
+                <section class="col-12" id="liste">
+                    <div class="row">
+                        <article class="card" style="width: 18rem;">
+                            <img src="..." class="card-img-top" alt="...">
+                            <div class="card-body">
+                              <h5 class="card-title">Card title</h5>
+                              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                              <a href="#" class="btn btn-primary">Go somewhere</a>
+                            </div>
+                        </article>
+                    </div>
+                </section>
+            </main>
+        </div>
+        <script src="monjs.js"></script>
+    </body>
+</html>
